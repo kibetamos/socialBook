2ac1d3bae5a3cf2ea04d7c2679422f913ec1556a
kibet kilel <amos.rotich@strathmore.edu>
2022-04-30 13:44:07 +0300

    Create view

--- social_book/core/views.py
+++ social_book/core/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from httplib2 import Http
 
 # Create your views here.
+
+def index(request):
+    return HttpResponse('<h1>Welcome home </h1>')
