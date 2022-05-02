0c5fd6d33503cf3d8076ff39dca5a4f0b6e80163
kibet kilel <amos.rotich@strathmore.edu>
2022-05-02 14:52:24 +0300

    Make login required

--- social_book/core/views.py
+++ social_book/core/views.py
@@ -3,10 +3,12 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.models import User, auth
 from django.contrib import messages
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
+
 
 
 # Create your views here.
-
+@login_required(login_url='signin')
 def index(request):
     return render(request,'index.html')
 
@@ -62,3 +64,7 @@ def signin(request):
 
     else:
         return render(request, 'signin.html')
+@login_required(login_url='signin')
+def logout(request):
+    auth.logout(request)
+    return redirect('signin')
