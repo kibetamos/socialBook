e0eeae89f7b4c1d751233150e6808ab454e7aa2c
kibet kilel <amos.rotich@strathmore.edu>
2022-05-02 14:38:00 +0300

    Login functionality

--- social_book/core/urls.py
+++ social_book/core/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('signup', views.signup, name='signup'),
-    path('login', views.login, name='login')
+    path('signin', views.signin, name='signin')
 
 
 ]
--- social_book/core/views.py
+++ social_book/core/views.py
@@ -43,5 +43,22 @@ def signup(request):
     else:
         return render(request, 'signup.html')
 
-def login(request):
-    return render(request, 'login.html')
+def signin(request):
+
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(username=username, password=password)
+
+        if  user is not None:
+            auth.login(request,user)
+            return redirect ('/')
+
+        else:
+            messages.info(request,'Credentials Invalid')
+            return redirect('signin')
+
+
+    else:
+        return render(request, 'signin.html')
