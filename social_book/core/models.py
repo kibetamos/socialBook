660093397d0d6ea2c10baf248a37fe771cf0980f
kibet kilel <amos.rotich@strathmore.edu>
2022-05-03 21:31:34 +0300

    Edit the setting.html file

--- social_book/core/models.py
+++ social_book/core/models.py
@@ -1,3 +1,4 @@
+from email.policy import default
 from django.db import models
 from django.contrib.auth import get_user_model
 
@@ -8,7 +9,7 @@ class Profile(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     id_user = models.IntegerField()
     bio = models.TextField(blank=True)
-    profileimg= models.ImageField(upload_to = 'profile_images')
+    profileimg = models.ImageField(upload_to='profile_images', default='blank-profile-picture.png')
     Location = models.CharField(max_length=100, blank=True)
 
     def __str__(self):
